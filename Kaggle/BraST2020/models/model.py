df3e51a45d3d87fc7f88cfcb5e37970ab7e9d61e
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-04-18 03:43:24 +0000

    2020 end

--- Kaggle/BraST2020/models/model.py
+++ Kaggle/BraST2020/models/model.py
@@ -2,239 +2,154 @@ import torch.nn as nn
 import torch 
 import torch.nn.functional as F
 
+from monai.networks.blocks import MemoryEfficientSwish
 
-from easydict import EasyDict 
 
-class VNet(nn.Module):
-    def __init__(self, args=None) -> None:
-        super(VNet, self).__init__()
+class DeepSEED(nn.Module):
+    def __init__(self, in_dim, hidden_dims, out_dim, dropout_p=0.1) -> None:
+        super(DeepSEED, self).__init__()
 
-        self.encoder = CompressionPath(args)
-        self.decoder = DecompressionPath(args)
+        self.encoder = Encoder(input_dim=in_dim, hidden_dim=hidden_dims, dropout_p=dropout_p)
+        self.decoder = Decoder(hidden_dim=hidden_dims, out_dim=out_dim, dropout_p=dropout_p)
 
-    
-    def forward(self, x, activate=False):
+    def forward(self, x):
 
         enc_out, stage_outputs = self.encoder(x)
-        dec_out = self.decoder(enc_out, stage_outputs)
+        out = self.decoder(enc_out, stage_outputs)
+
+        return out
+class SqueezeExcitation3D(nn.Module):
+    def __init__(self, in_dim, reduction_ratio=4) -> None:
+        super(SqueezeExcitation3D, self).__init__()
+
+        self.squeeze = nn.AdaptiveAvgPool3d(1) # 1x1x1xC
+
+        self.excitation = nn.Sequential(
+            nn.Conv3d(in_channels=in_dim, out_channels=in_dim//reduction_ratio, kernel_size=1, stride=1, bias=False),
+            nn.SiLU(),
+            nn.Conv3d(in_channels=in_dim//reduction_ratio, out_channels=in_dim, kernel_size=1, stride=1, bias=False),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+
+        se_out = self.squeeze(x)
+
+        se_out = self.excitation(se_out)
+
+        return x * se_out
 
 
-        if not activate:
-            return dec_out
-        else:
-            output = F.softmax(dec_out, dim=1)
-        return output
 
-# Encoder; 'compression path'
-# ... The left part of the network consists of a compress, ...
-class CompressionPath(nn.Module):
-    def __init__(self, args=None) -> None:
-        super(CompressionPath, self).__init__()
+class FusedMBConv3d(nn.Module):
+    def __init__(self, in_dim, out_dim, expansion_ratio=4, squeeze_ratio=4, stride=1, dropout_p=0.1) -> None:
+        super(FusedMBConv3d, self).__init__()
+        self.use_residual = in_dim == out_dim and stride == 1 
+        hidden_dim = int(in_dim * expansion_ratio)
+        padding = 1
 
-        if args is None:
-            args = EasyDict()
-            args.in_dim  = 4
-            args.net_dim = [16, 32, 64, 128, 256]
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels=in_dim, out_channels=hidden_dim, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.InstanceNorm3d(hidden_dim),
+            nn.SiLU(),
+            nn.Dropout3d(p=dropout_p)
+        )
 
-            conv_kwargs = EasyDict()
-            conv_kwargs.kernel_size = (5,5,5)
-            conv_kwargs.stride      = (1,1,1)
-            conv_kwargs.padding     = 2
+        self.se = SqueezeExcitation3D(in_dim=hidden_dim, reduction_ratio=squeeze_ratio)
 
-            down_kwargs = EasyDict()
-            down_kwargs.kernel_size = (2,2,2)
-            down_kwargs.stride      = (2,2,2)
-            down_kwargs.padding     = 0
+        self.projection = nn.Sequential(
+            nn.Conv3d(in_channels=hidden_dim, out_channels=out_dim, kernel_size=1, stride=1, bias=False),
+            nn.InstanceNorm3d(out_dim),
+            nn.Dropout3d(p=dropout_p)
+        )
+    
+    def forward(self, x):
+        h = self.conv(x)
+        h = self.se(h)
+        h = self.projection(h)
 
-            args.conv_kwargs = conv_kwargs
-            args.down_kwargs = down_kwargs
-
-        self.conv1 = nn.Sequential(
-                nn.Conv3d(args.in_dim, args.net_dim[0], kernel_size=5, stride=1, padding=2),
-                nn.SiLU()
-            )
-        self.down1 = nn.Sequential(
-                nn.Conv3d(args.net_dim[0], args.net_dim[1], **args.down_kwargs),
-                nn.SiLU()
-            )
-
-        self.conv2 = nn.Sequential(
-                nn.Conv3d(args.net_dim[1], args.net_dim[1], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[1], args.net_dim[1], **args.conv_kwargs),
-                nn.SiLU()
-            )
-        self.down2 = nn.Sequential(
-                nn.Conv3d(args.net_dim[1], args.net_dim[2], **args.down_kwargs),
-                nn.SiLU()
-            )
+        if self.use_residual:
+            h = h + x
         
-        self.conv3 = nn.Sequential(
-                nn.Conv3d(args.net_dim[2], args.net_dim[2], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[2], args.net_dim[2], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[2], args.net_dim[2], **args.conv_kwargs),
-                nn.SiLU()
-            )
-        self.down3 = nn.Sequential(
-                nn.Conv3d(args.net_dim[2], args.net_dim[3], **args.down_kwargs),
-                nn.SiLU()
-            )
+        return h
+    
 
-        self.conv4 = nn.Sequential(
-                nn.Conv3d(args.net_dim[3], args.net_dim[3], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[3], args.net_dim[3], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[3], args.net_dim[3], **args.conv_kwargs),
-                nn.SiLU()
-            )
-        self.down4 = nn.Sequential(
-                nn.Conv3d(args.net_dim[3], args.net_dim[4], **args.down_kwargs),
-                nn.SiLU()
-            )
 
-        self.conv5 = nn.Sequential(
-                nn.Conv3d(args.net_dim[4], args.net_dim[4], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[4], args.net_dim[4], **args.conv_kwargs),
-                nn.SiLU(),
-                nn.Conv3d(args.net_dim[4], args.net_dim[4], **args.conv_kwargs),
-                nn.SiLU()
-            )
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, dropout_p=0.1) -> None:
+        super(Encoder, self).__init__()
+
+        # layers = 
+        self.conv1 = nn.Sequential(*[FusedMBConv3d(in_dim=input_dim, out_dim=hidden_dim[0], stride=2, dropout_p=dropout_p)]+ \
+              [FusedMBConv3d(in_dim=hidden_dim[0], out_dim=hidden_dim[0], stride=1, dropout_p=dropout_p) for _ in range(1)])
+
+        self.conv2 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[0], out_dim=hidden_dim[1], stride=2, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[1], out_dim=hidden_dim[1], stride=1, dropout_p=dropout_p) for _ in range(2)]
+        )
+
+        self.conv3 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[1], out_dim=hidden_dim[2], stride=2, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[2], out_dim=hidden_dim[2], stride=1, dropout_p=dropout_p) for _ in range(3)]
+        )
+
+        self.conv4 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[2], out_dim=hidden_dim[3], stride=2, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[3], out_dim=hidden_dim[3], stride=1, dropout_p=dropout_p) for _ in range(4)]
+        )
+
+    def forward(self, x):
+
+        h1 = self.conv1(x)
+        h2 = self.conv2(h1)
+        h3 = self.conv3(h2)
+        h4 = self.conv4(h3)
+
+        stage_outputs = [h1, h2, h3]
+
+        return h4, stage_outputs
+    
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_dim, out_dim, dropout_p=0.1):
+        super(Decoder, self).__init__()
 
         
+        self.up_conv1 = nn.ConvTranspose3d(hidden_dim[3], hidden_dim[2], kernel_size=2, stride=2)
+        self.conv1 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[3], out_dim=hidden_dim[2], stride=1, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[2], out_dim=hidden_dim[2], stride=1, dropout_p=dropout_p) for _ in range(4) ]
+        )
 
-    def forward(self, x) :
-        
-        h1  = self.conv1(x)
-        d1  = self.down1(h1)
+        self.up_conv2 = nn.ConvTranspose3d(hidden_dim[2], hidden_dim[1], kernel_size=2, stride=2)
+        self.conv2 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[2], out_dim=hidden_dim[1], stride=1, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[1], out_dim=hidden_dim[1], stride=1, dropout_p=dropout_p) for _ in range(3) ]
+        )
 
-        id2 = d1
-        h2  = self.conv2(d1)
-        h2  = h2 + id2
-        d2  = self.down2(h2)
+        self.up_conv3 = nn.ConvTranspose3d(hidden_dim[1], hidden_dim[0], kernel_size=2, stride=2)
+        self.conv3 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[1], out_dim=hidden_dim[0], stride=1, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[0], out_dim=hidden_dim[0], stride=1, dropout_p=dropout_p) for _ in range(2) ]
+        )
 
-        id3 = d2 
-        h3  = self.conv3(d2)
-        h3  = h3 + id3 
-        d3  = self.down3(h3)
-
-        id4 = d3 
-        h4  = self.conv4(d3)
-        h4  = h4 + id4
-        d4  = self.down4(h4)
-
-        id5 = d4
-        h5  = self.conv5(d4)
-        h5  = h5 + id5
-
-        stage_outputs = [h1, h2, h3, h4] # forward the features extracted from early stages of the left part of the CNN to the right part
-
-        return h5, stage_outputs
-
-# decoder; 'decompression path'
-# while the right part decompresses the signal until its original size is reached.
-class DecompressionPath(nn.Module):
-    def __init__(self, args=None) -> None:
-        super(DecompressionPath, self).__init__()
-
-        if args is None:
-            args = EasyDict()
-            args.out_dim = 2
-            args.net_dim = [16, 32, 64, 128, 256]
-
-            conv_kwargs = EasyDict()
-            conv_kwargs.kernel_size = (5,5,5)
-            conv_kwargs.stride      = (1,1,1)
-            conv_kwargs.padding     = 2
-
-            up_kwargs = EasyDict()
-            up_kwargs.kernel_size = (2,2,2)
-            up_kwargs.stride      = (2,2,2)
-            up_kwargs.padding     = 0
-
-            args.conv_kwargs = conv_kwargs
-            args.up_kwargs   = up_kwargs
-            
-
-        self.up1 = nn.Sequential(
-                nn.ConvTranspose3d(args.net_dim[-1], args.net_dim[-2], **args.up_kwargs),
-                nn.PReLU()
-            )
-        self.conv1 = nn.Sequential(
-                nn.Conv3d(args.net_dim[-1], args.net_dim[-1], **args.conv_kwargs),
-                nn.PReLU(),
-                nn.Conv3d(args.net_dim[-1], args.net_dim[-1], **args.conv_kwargs),
-                nn.PReLU(),
-                nn.Conv3d(args.net_dim[-1], args.net_dim[-1], **args.conv_kwargs),
-                nn.PReLU()
-            )
-
-        self.up2 = nn.Sequential(
-                nn.ConvTranspose3d(args.net_dim[-2], args.net_dim[-3], **args.up_kwargs),
-                nn.PReLU()
-            )
-        self.conv2 = nn.Sequential(
-                nn.Conv3d(args.net_dim[-2], args.net_dim[-2], **args.conv_kwargs),
-                nn.PReLU(),
-                nn.Conv3d(args.net_dim[-2], args.net_dim[-2], **args.conv_kwargs),
-                nn.PReLU(),
-                nn.Conv3d(args.net_dim[-2], args.net_dim[-2], **args.conv_kwargs),
-                nn.PReLU()
-            )
-        
-        self.up3 = nn.Sequential(
-                nn.ConvTranspose3d(args.net_dim[-3], args.net_dim[-4], **args.up_kwargs),
-                nn.PReLU()
-            )
-        self.conv3 = nn.Sequential(
-                nn.Conv3d(args.net_dim[-3], args.net_dim[-3], **args.conv_kwargs),
-                nn.PReLU(),
-                nn.Conv3d(args.net_dim[-3], args.net_dim[-3], **args.conv_kwargs),
-                nn.PReLU()
-            )
-
-        self.up4 = nn.Sequential(
-                nn.ConvTranspose3d(args.net_dim[-4], args.net_dim[-5], **args.up_kwargs),
-                nn.PReLU()
-            )
-        self.conv4 = nn.Sequential(
-                nn.Conv3d(args.net_dim[-4], args.net_dim[-4], **args.conv_kwargs),
-                nn.PReLU()
-            )
-
-        self.conv5 = nn.Sequential(
-                nn.Conv3d(args.net_dim[-4], args.out_dim, kernel_size=1, stride=1, padding=0),
-                nn.PReLU()
-            )
+        self.up_conv4 = nn.ConvTranspose3d(hidden_dim[0], hidden_dim[0], kernel_size=2, stride=2)
+        self.conv4 = nn.Sequential(*[FusedMBConv3d(in_dim=hidden_dim[0], out_dim=hidden_dim[0], stride=1, dropout_p=dropout_p)] \
+              +[FusedMBConv3d(in_dim=hidden_dim[0], out_dim=out_dim, stride=1, dropout_p=dropout_p) for _ in range(1) ]
+        )
 
     def forward(self, enc_out, stage_outputs):
-        
-            u1  = self.up1(enc_out)
-            id1 = u1
-            h1  = torch.cat([u1, stage_outputs[-1]], dim=1)
-            h1  = self.conv1(h1)
 
-            u2  = self.up2(h1)
-            id2 = u2
-            h2  = torch.cat([u2, stage_outputs[-2]], dim=1)
-            h2  = self.conv2(h2)
-            h2  = h2 + id2
+        h1 = self.up_conv1(enc_out)
+        h1 = torch.concat([h1, stage_outputs[-1]], dim=1)
+        h1 = self.conv1(h1)
 
-            u3  = self.up3(h2)
-            id3 = u3
-            h3  = torch.cat([u3, stage_outputs[-3]], dim=1)
-            h3  = self.conv3(h3)
-            h3  = h3 + id3
+        h2 = self.up_conv2(h1)
+        h2 = torch.concat([h2, stage_outputs[-2]], dim=1)
+        h2 = self.conv2(h2)
 
-            u4  = self.up4(h3)
-            id4 = u4
-            h4  = torch.cat([u4, stage_outputs[-4]], dim=1)
-            h4  = self.conv4(h4)
-            h4  = h4 + id4
+        h3 = self.up_conv3(h2)
+        h3 = torch.concat([h3, stage_outputs[-3]], dim=1)
+        h3 = self.conv3(h3)
 
-            out = self.conv5(h4)
+        h4 = self.up_conv4(h3)
+        h4 = self.conv4(h4)
 
-            return out
+        return h4
+
+    
+
