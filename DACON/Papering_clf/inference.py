07f5fc6f7529203feebe8c2f33e1b2491ff80b99
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-05-01 04:34:59 +0000

    changed logging from

--- DACON/Papering_clf/inference.py
+++ DACON/Papering_clf/inference.py
@@ -16,7 +16,7 @@ from model.models import BaseModel
 args = EasyDict()
 
 args.img_size = 384
-args.batch_size = 32
+args.batch_size = 8
 
 
 test_transform = A.Compose([
@@ -27,15 +27,15 @@ test_transform = A.Compose([
 
 le = preprocessing.LabelEncoder()
 
-all_img_list = glob.glob('./data/train/*/*')
+all_img_list = glob.glob('./aug_data/train/*/*')
 df = pd.DataFrame(columns=['img_path', 'label'])
 df['img_path'] = all_img_list
 df['label'] = df['img_path'].apply(lambda x : str(x).split('/')[-2])
 df['label'] = le.fit_transform(df['label'])
 
-test = pd.read_csv('/root/Competitions/DACON/Papering_clf/data/test.csv')
+test = pd.read_csv('/root/Competitions/DACON/Papering_clf/aug_data/test.csv')
 path = test['img_path'].values
-path = [f'/root/Competitions/DACON/Papering_clf/data/test/{file.split("/")[-1]}' for file in path]
+path = [f'/root/Competitions/DACON/Papering_clf/aug_data/test/{file.split("/")[-1]}' for file in path]
 
 
 test_dataset = CustomDataset(path, None, test_transform)
@@ -43,10 +43,10 @@ test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False
 
 model = BaseModel(19)
 
-ckpt = '/root/Competitions/DACON/Papering_clf/lightning_logs/ConvNeXt_t, Geo+value || lr=[8e-05], img_size = [384], bz=[128]/checkpoints/BaseModel-epoch=055-train_loss=0.8598-avg_f1=0.8759.ckpt'
+ckpt = '/root/Competitions/DACON/Papering_clf/lightning_logs/[S.7] --m ConvNeXt, --d A, --t GV --L F || lr=[8e-05], img=[368], bz=[16]/checkpoints/BaseModel-epoch=059-train_loss=0.7788-avg_f1=0.8711.ckpt'
 pl_runner = LightningRunner.load_from_checkpoint(ckpt, network=model, args=args)
 
-DEVICE = 'cuda:0'
+DEVICE = 'cuda:2'
 pl_runner.to(DEVICE)
 pl_runner.eval()
 
@@ -60,9 +60,9 @@ for x in tqdm(test_loader):
     del x 
 
 inferences = le.inverse_transform(inferences)
-submit = pd.read_csv('/root/Competitions/DACON/Papering_clf/data/sample_submission.csv')
+submit = pd.read_csv('/root/Competitions/DACON/Papering_clf/aug_data/sample_submission.csv')
 submit['label'] = inferences
-submit.to_csv('/root/Competitions/DACON/Papering_clf/prediction/ConvNeXt_t, Geo+value || lr=[8e-05], img_size = [384], bz=[128](train_loss=0.8598-avg_f1=0.8759).csv', index=False)
+submit.to_csv('/root/Competitions/DACON/Papering_clf/prediction/[S.7] -m ConvNeXt, -d A, -t GV -L F || lr=[8e-05], img=[368], bz=[16](epc=059-loss=0.7788-f1=0.8711).csv', index=False)
     
 
 
