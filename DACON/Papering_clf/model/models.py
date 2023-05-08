ca68d123b451c1f5ce071e22443a69a1ce49a459
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-05-08 12:02:06 +0000

    backbone

--- DACON/Papering_clf/model/models.py
+++ DACON/Papering_clf/model/models.py
@@ -1,14 +1,17 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from inceptionNeXt import inceptionnext_tiny
-
+# from inceptionNeXt import inceptionnext_tiny
+from transformers import ConvNextFeatureExtractor, ConvNextForImageClassification
 class BaseModel(nn.Module):
     def __init__(self, num_classes:int):
         super(BaseModel, self).__init__()
         
         # self.backbone = models.convnext_tiny(weights=models.ConvNeXt_Tiny_Weights.DEFAULT)
-        self.backbone = inceptionnext_tiny(pretrained=True)
+        # self.backbone = models.convnext_base(weights=models.ConvNeXt_Base_Weights.DEFAULT)
+        self.backbone = models.convnext_large(weights=models.ConvNeXt_Large_Weights.DEFAULT)
+        # self.backbone = ConvNextForImageClassification.from_pretrained("facebook/convnext-base-384-224-1k")
+        # self.backbone = inceptionnext_tiny(pretrained=True)
         # self.backbone = models.swin_v2_t(weights=models.Swin_V2_T_Weights.DEFAULT)
         # self.backbone = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
         self.norm = nn.LayerNorm(1000)
