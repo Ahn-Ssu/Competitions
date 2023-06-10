1a38dbd9dbbcf9c7f1584718492e2da4763274c2
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-06-10 04:15:06 +0000

    models.py

--- DACON/Papering_clf/model/models.py
+++ DACON/Papering_clf/model/models.py
@@ -1,19 +1,11 @@
-import torch
 import torch.nn as nn
 import torchvision.models as models
-# from inceptionNeXt import inceptionnext_tiny
-from transformers import ConvNextFeatureExtractor, ConvNextForImageClassification
+
 class BaseModel(nn.Module):
     def __init__(self, num_classes:int):
         super(BaseModel, self).__init__()
         
-        # self.backbone = models.convnext_tiny(weights=models.ConvNeXt_Tiny_Weights.DEFAULT)
-        # self.backbone = models.convnext_base(weights=models.ConvNeXt_Base_Weights.DEFAULT)
         self.backbone = models.convnext_large(weights=models.ConvNeXt_Large_Weights.DEFAULT)
-        # self.backbone = ConvNextForImageClassification.from_pretrained("facebook/convnext-base-384-224-1k")
-        # self.backbone = inceptionnext_tiny(pretrained=True)
-        # self.backbone = models.swin_v2_t(weights=models.Swin_V2_T_Weights.DEFAULT)
-        # self.backbone = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
         self.norm = nn.LayerNorm(1000)
         self.act = nn.SiLU()
         self.drop = nn.Dropout1d()
@@ -26,3 +18,12 @@ class BaseModel(nn.Module):
         x = self.drop(x)
         x = self.classifier(x)
         return x
+    
+    
+# def replace_module(modules:nn.Module, target, source):
+#         for name, child in modules.named_children():
+#             if isinstance(child, target):
+#                 modules._modules[name] = source()
+#             # elif isinstance(child, nn.Sequential):
+#             else: 
+#                 replace_module(child, target, source)
