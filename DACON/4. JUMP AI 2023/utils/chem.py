a8fd0622d33398d30ee1985274d4c5abf123bef1
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-08-28 15:38:31 +0000

    fixed type err

--- DACON/4. JUMP AI 2023/utils/chem.py
+++ DACON/4. JUMP AI 2023/utils/chem.py
@@ -2,6 +2,9 @@ from mendeleev.fetch import fetch_table
 from rdkit import Chem
 from rdkit.Chem import Descriptors, Lipinski, Crippen, rdMolDescriptors, MolFromSmiles
 
+from torch import Tensor
+from torch_geometric.utils.sparse import dense_to_sparse
+
 import numpy as np
 
 
@@ -16,12 +19,15 @@ class Chemical_feature_generator():
                         # Is Radioactive: This binary property may not be directly relevant to metabolic stability.
                         # Molar Heat Capacity, Specific Heat Capacity: These properties relate to heat transfer but might not be directly tied to metabolic stability.
         self.mendeleev_atomic_f_table = fetch_table('elements')[mendeleev_atomic_f]
+        
     
 
     def get_atomic_features(self,atom):
 
         atomic_num = atom.GetAtomicNum() - 1 # -1 is offset
         mendel_atom_f = self.mendeleev_atomic_f_table.loc[atomic_num]
+        mendel_atom_f.is_radioactive = mendel_atom_f.is_radioactive.astype(int)
+        mendel_atom_f = mendel_atom_f.to_numpy().astype(np.float32)
 
         rdkit_atom_f = [atom.GetDegree(),
                 atom.GetTotalDegree(),
@@ -123,7 +129,7 @@ class Chemical_feature_generator():
 
     
 
-    def generate_molecular_features(self, smiles):
+    def generate_mol_atomic_features(self, smiles):
 
         mol = Chem.MolFromSmiles(smiles)
 
@@ -136,42 +142,57 @@ class Chemical_feature_generator():
             mendel_atom_features.append(mendel_atom_f)
             rdkit_atom_features.append(rdkit_atom_f)
 
-        molecular_features = self.get_molecular_features(mol)
-        atomic_features = np.concatenate([mendel_atom_features, rdkit_atom_features], axis=1)
+        atomic_features = np.concatenate([mendel_atom_features, rdkit_atom_features], axis=1, dtype=np.float32)
         
-        return atomic_features, molecular_features
+        return atomic_features
+    
+    def get_adj_matrix(self, smiles):
+        
+        mol = MolFromSmiles(smiles)
+        adj = Chem.rdmolops.GetAdjacencyMatrix(mol)
+        
+        edge_index, edge_attr = dense_to_sparse(Tensor(adj))
+        
+        bonds = []
+        for i in range(0, mol.GetNumAtoms()):
+            for j in range(0, mol.GetNumAtoms()):
+                if adj[i, j] == 1:
+                    bonds.append([i, j])
+            
+        return edge_index, edge_attr
 
 
 if __name__ == '__main__' : 
     import pandas as pd 
     from tqdm import tqdm
     
-    train = pd.read_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/train.csv')
-    test  = pd.read_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/test.csv')
+    train = pd.read_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_train.csv')
+    test  = pd.read_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_test.csv')
     
     generator = Chemical_feature_generator()
-    
-    
     
     def process(df):
         molecular_f = [] 
         for sample in tqdm(df.SMILES):
             _, molecular_features = generator.generate_molecular_features(smiles=sample)
+            generator.get_adj_matrix(sample)
             molecular_f.append(molecular_features)
-            
-        molecular_f = np.concatenate([molecular_f], axis=0)
-        print(molecular_f.shape)
+            break
+    #     molecular_f = np.concatenate([molecular_f], axis=0)
+    #     print(molecular_f.shape)
         
-        return pd.DataFrame(data=molecular_f, columns=['MolWt','HeavyAtomMolWt','NumValenceElectrons','FractionCSP3','HeavyAtomCount','NHOHCount','NOCount','NumAliphaticCarbocycles','NumAliphaticHeterocycles','NumAliphaticRings','NumAromaticCarbocycles','NumAromaticHeterocycles','NumAromaticRings','NumHAcceptors','NumHDonors','NumHeteroatoms','NumRotatableBonds','RingCount','MolMR','CalcNumBridgeheadAtom','ExactMolWt','NumRadicalElectrons','NumSaturatedCarbocycles','NumSaturatedHeterocycles','NumSaturatedRings','MolLogP','CalcNumAmideBonds','CalcNumSpiroAtoms','num_carboxyl_groups','num_amion_groups','num_ammonium_groups','num_sulfonic_acid_groups','num_alkoxy_groups'])
+    #     return pd.DataFrame(data=molecular_f, columns=['MolWt','HeavyAtomMolWt','NumValenceElectrons','FractionCSP3','HeavyAtomCount','NHOHCount','NOCount','NumAliphaticCarbocycles','NumAliphaticHeterocycles','NumAliphaticRings','NumAromaticCarbocycles','NumAromaticHeterocycles','NumAromaticRings','NumHAcceptors','NumHDonors','NumHeteroatoms','NumRotatableBonds','RingCount','MolMR','CalcNumBridgeheadAtom','ExactMolWt','NumRadicalElectrons','NumSaturatedCarbocycles','NumSaturatedHeterocycles','NumSaturatedRings','MolLogP','CalcNumAmideBonds','CalcNumSpiroAtoms','num_carboxyl_groups','num_amion_groups','num_ammonium_groups','num_sulfonic_acid_groups','num_alkoxy_groups'])
     
-    train_molecular_f = process(train)
-    train_merged = pd.concat([train, train_molecular_f], axis=1)
+    print(train.iloc[1, :])
+    print(test.columns)
+    # train_molecular_f = process(train)
+    # train_merged = pd.concat([train, train_molecular_f], axis=1)
     
-    test_molecular_f = process(test)
-    test_merged = pd.concat([test, test_molecular_f], axis=1)
+    # test_molecular_f = process(test)
+    # test_merged = pd.concat([test, test_molecular_f], axis=1)
     
-    print(train_merged.shape)
-    print(test_merged.shape)
+    # print(train_merged.shape)
+    # print(test_merged.shape)
     
-    train_merged.to_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_train.csv')
-    test_merged.to_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_test.csv')
+    # train_merged.to_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_train.csv')
+    # test_merged.to_csv('/root/Competitions/DACON/4. JUMP AI 2023/data/new_test.csv')
